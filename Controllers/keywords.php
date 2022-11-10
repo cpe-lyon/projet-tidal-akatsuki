a7f238a6d690f6800b32ff76db013d69f73d860a
pjkaberuka <93118177+pjkaberuka@users.noreply.github.com>
2022-11-10 16:48:21 +0100

    Update and rename keywords to keywords.php

--- Controllers/keywords.php
+++ Controllers/keywords.php
@@ -13,8 +13,14 @@
         die('Erreur : '.$e->getMessage());     // En cas d'erreur, on affiche un message et on arrête tout 
     }
 
-    $reponse = $bdd->query('SELECT * FROM patho');
+    $reponse = $bdd->prepare('SELECT * FROM patho');
     $reponse->execute();
+
+    session_start();
+    if(!isset($_SESSION['email'])){
+        header('Location: connexion.php');
+        exit();
+    }
 ?>
 
 <html lang="fr">
@@ -24,6 +30,7 @@
     </head>
 
     <body>
+    <a href="deconnexion.php">Déconexion</a>
         <h2>recherche de pathologie par mot-clef</h2>
         
         <form method="GET" action="keywords.php">
@@ -36,25 +43,29 @@
     if(isset($_GET['q']) AND !empty($_GET['q'])) {
         $q = htmlspecialchars($_GET['q']);
         $elem_page = 10;
-        $requete_elem_total = $bdd->query('SELECT distinct pt.desc FROM patho pt,symptpatho sp,symptome sy,keySympt ks,keywords kw WHERE kw.name LIKE \'%'.$q.'%\' AND kw.idK=ks.idK AND ks.idS=sy.idS AND sy.idS=sp.idS AND sp.idP=pt.idP');
-        $elem_total = $requete_elem_total->rowCOUNT();
+        $existe = $bdd->prepare('SELECT distinct kw.name FROM keywords kw WHERE kw.name LIKE ?');
+        $existe->execute([$q]); 
+        $executeexiste = $existe->fetch();
+        if($executeexiste){
+            $requete_elem_total = $bdd->query('SELECT distinct pt.desc FROM patho pt,symptpatho sp,symptome sy,keySympt ks,keywords kw WHERE kw.name LIKE \'%'.$q.'%\' AND kw.idK=ks.idK AND ks.idS=sy.idS AND sy.idS=sp.idS AND sp.idP=pt.idP');
+            $elem_total = $requete_elem_total->rowCOUNT();
 
 
-        if(isset($_GET['page']) AND !empty($_GET['page']) AND $_GET['page'] > 0)
+            if(isset($_GET['page']) AND !empty($_GET['page']) AND $_GET['page'] > 0)
             {
                 $_GET['page'] = intval ($_GET['page']);
                 $pageCourante = $_GET['page'];
             }
 
-        else
+            else
             {
                 $pageCourante = 1;
             }
         
-        $depart = ($pageCourante-1) * $elem_page;
-        $page_total = ceil ($elem_total/ $elem_page);
-        $requete = $bdd->query('SELECT distinct pt.desc FROM patho pt,symptpatho sp,symptome sy,keySympt ks,keywords kw WHERE kw.name LIKE \'%'.$q.'%\' AND kw.idK=ks.idK AND ks.idS=sy.idS AND sy.idS=sp.idS AND sp.idP=pt.idP ORDER BY pt.desc OFFSET '.$depart.' LIMIT '.$elem_page.'');
-        
+            $depart = ($pageCourante-1) * $elem_page;
+            $page_total = ceil ($elem_total/ $elem_page);
+            $requete = $bdd->query('SELECT distinct pt.desc FROM patho pt,symptpatho sp,symptome sy,keySympt ks,keywords kw WHERE kw.name LIKE \'%'.$q.'%\' AND kw.idK=ks.idK AND ks.idS=sy.idS AND sy.idS=sp.idS AND sp.idP=pt.idP ORDER BY pt.desc OFFSET '.$depart.' LIMIT '.$elem_page.'');
+            
             
 
             while ($a = $requete ->fetch()){     
@@ -68,7 +79,14 @@
             </ul>
             <?php
              }
-         }
+        }
+        else{
+            echo 'Aucun résultat pour le mot clef saisis';
+        }
+    }else{
+        echo 'Veuillez entrez un mot clef';
+    }
+        
          
  ?>
 
