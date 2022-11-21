23490c13ab60d5827a80f924f7482bbe2c93c321
pjkaberuka <93118177+pjkaberuka@users.noreply.github.com>
2022-11-21 17:26:45 +0100

    Update m_keywords.php

--- models/m_keywords.php
+++ models/m_keywords.php
@@ -1,34 +1,23 @@
 <?php
+
+include_once(PATH_MODELS . 'model.php');
 class Keywords
 {
-    private $bdd;
     function getPatho(){
-        $servername = 'localhost';
-        $username = 'pgtp';
-        $password = 'tp';
-        
-        try {
-            // connection à la base de données 
-            $this->$bdd = new PDO("pgsql:host=$servername;dbname=WEBSITE", $username, $password); 
-        }
-
-        catch(Exception $e) {
-            die('Erreur : '.$e->getMessage());     // En cas d'erreur, on affiche un message et on arrête tout 
-        }
-
-        $reponse = $this->$bdd->prepare('SELECT * FROM patho');
+        $bdd = getConnection();
+        $reponse = $bdd->query('SELECT * FROM patho');
         $reponse->execute();
 
-        //session_start();
-        // if(!isset($_SESSION['email'])){
-        //     header('Location: c_connexion.php');
-        //     exit();
-        // }
+        session_start();
+        if(!isset($_SESSION['email'])){
+            header('Location: index.php?page=connexion');
+            exit();
+        }
 
         return $reponse;
     }
 
-    function sum(){
+    function getExecute(){
         // MVC à faire
     }
 }
