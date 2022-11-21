f1e93a7e036e3ee965617bd5067843922f61b9d3
pjkaberuka <93118177+pjkaberuka@users.noreply.github.com>
2022-11-21 17:24:58 +0100

    Update v_patho2.php

--- views/v_patho2.php
+++ views/v_patho2.php
@@ -1,44 +1,10 @@
-<html lang="fr">
-
-<head>
-    <title>Pathologie</title>
-    <link rel="stylesheet" href="../assets/index.css">
-    <script src="../assets/script.js"> </script>
-</head>
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">Menu</a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-            <li class="nav-item active">
-                <a class="nav-link" href="../index.php">Accueil<span class="sr-only">Recherche</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="../controllers/c_patho.php">Recherche Pathologie<span class="sr-only">Recherche</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="../controllers/c_patho2.php">Filtre Pathologie<span class="sr-only">Recherche</span></a>
-            </li>
-            <li class="nav-item active">
-                <a class="nav-link" href="../controllers/c_keywords.php">Recherche par mot-clef</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">A propos</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="c_deconnexion.php">Déconexion</a>
-            </li>
-        </ul>
-    </div>
-</nav>
+<?php require_once(PATH_VIEWS . 'header.php'); ?>
 </br>
+
 <h1 class="title">Pathologie n°2</h1>
 
-<body onload="afficher()">
-    <form method="GET" action="c_patho2.php">
+<div onload="afficher()">
+    <form method="POST" action="index.php?page=patho2">
         <SELECT name="input" id="input">
             <option value="" selected>Choisir un critère</option>
             <option value="affichertype">type</option>
@@ -77,6 +43,7 @@
     </form>
 
     <?php
+    if($reponsePatho2 != ""){
         if ((empty($reponsePatho2->fetch()))) {
             echo "Aucune pathologie n'a été trouvé pour les critères définis";
         } else {
@@ -92,8 +59,8 @@
     <?php
             }
         }
+    }
     ?>
- 
-</body>
+</div>
 
-</html>
+<?php require_once(PATH_VIEWS . 'footer.php'); ?>
